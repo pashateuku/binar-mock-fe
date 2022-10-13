50f7d18a28c7194a3c7df4c1ff9102df42f3f781
pashateuku <thariqteuku.soc@gmail.com>
2022-10-13 20:30:06 +0700

    add: form validation and register page

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,24 +1,47 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../assets/css/Login.css';
 
+import {useState} from 'react';
+
 function App() {
+  const [value, setValue] = useState('');
+
+  const handleNumericInput = event => {
+    const result = event.target.value.replace(/\D/g, '');
+
+    setValue(result);
+  };
+
   return (
     <header className='login-page'>
       <div className='row form-parent'>
         <div className="col-10 col-sm-7 col-md-5 col-lg-4 col-xl-3 form-content">
           
           <div className='form-content-header'>
-            <h2>📝todo</h2>
-            <p>productive together</p>
+            <h2>✔simpledos</h2>
+            <p>simply productive</p>
           </div>
           
           <form className='form-content-input'>
 
             <p className='form-content-input__input-text'>4-digit UserID</p>
-            <input className='form-content-input__input-form'></input>
+            <input 
+              className='form-content-input__input-form'
+              maxlength="4"
+              id="user_4id"
+              name="user_4id"
+              value={value}
+              onChange={handleNumericInput}
+            />
 
             <p className='form-content-input__input-text'>Password</p>
-            <input className='form-content-input__input-form'></input>
+            <input
+              className='form-content-input__input-form'
+              type="password"
+              id="password"
+              name="password"
+              minlength="8"
+            />
 
             <button className='form-content-input__button-form'>Log In</button>
 
--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,22 +1,57 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../assets/css/Register.css';
 
+import {useState} from 'react';
+
 function App() {
+  const [value, setValue] = useState('');
+
+  const handleNumericInput = event => {
+    const result = event.target.value.replace(/\D/g, '');
+
+    setValue(result);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Ini Register Page 🔐
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className='register-page'>
+      <div className='row form-parent'>
+        <div className="col-10 col-sm-7 col-md-5 col-lg-4 col-xl-3 form-content">
+          
+          <div className='form-content-header'>
+            <h2>✔simpledos</h2>
+            <p>simply productive</p>
+          </div>
+          
+          <form className='form-content-input'>
+
+            <p className='form-content-input__input-text'>4-digit UserID</p>
+            <input 
+              className='form-content-input__input-form'
+              maxlength="4"
+              id="user_4id"
+              name="user_4id"
+              value={value}
+              onChange={handleNumericInput}
+            />
+
+            <p className='form-content-input__input-text'>Password</p>
+            <input
+              className='form-content-input__input-form'
+              type="password"
+              id="password"
+              name="password"
+              minlength="8"
+            />
+
+            <button className='form-content-input__button-form'>Create new Account</button>
+
+            <a className='form-content-input__href-register' href='/login'>Sign in</a>
+
+          </form>
+
+        </div>
+      </div>
+    </header>
   );
 }
 
